c19b8e4cc2959740be1c05efe4bc3ab470f3e769
Daniel Nweze <daniel.nweze@hotmail.com>
2025-05-21 19:27:40 +0200

    latest

--- EventApp/Program.cs
+++ EventApp/Program.cs
@@ -21,8 +21,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
             "http://localhost:5173",
-            "https://localhost:5173",
-            "https://<din-render-frontend-url>" // <-- lägg till detta vid behov
+            "https://localhost:5173"
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
