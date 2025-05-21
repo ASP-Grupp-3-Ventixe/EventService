02aa7d9d59f5a1e3e4da76133bf71ed87497ec1b
Daniel Nweze <daniel.nweze@hotmail.com>
2025-05-21 18:34:28 +0200

    added Docker

--- EventApp/Program.cs
+++ EventApp/Program.cs
@@ -8,10 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); 
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddScoped<IEventService, EventService>();
 
