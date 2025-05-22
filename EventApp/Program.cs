e62969edd09fd9a4f9a393a3879a45e06ad387a0
Daniel Nweze <daniel.nweze@hotmail.com>
2025-05-22 17:55:37 +0200

    please

--- EventApp/Program.cs
+++ EventApp/Program.cs
@@ -2,13 +2,13 @@ using EventApp;
 using EventApp.Interfaces;
 using EventApp.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -17,28 +17,38 @@ builder.Services.AddScoped<IEventService, EventService>();
 
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(policy =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins(
-            "http://localhost:5173", 
-            "https://localhost:5173",
-            "https://kind-coast-0cff2bc03.6.azurestaticapps.net/",
-            "https://ventixe-cave.netlify.app",
-           "https://ventixe-frontend.onrender.com"
-        )
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
+        policy
+            .SetIsOriginAllowed(_ => true)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
-
-
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
 var app = builder.Build();
 
+// SKAPA mappar om de saknas
+var staticFilesPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+if (!Directory.Exists(staticFilesPath))
+    Directory.CreateDirectory(staticFilesPath);
+
+var imagesPath = Path.Combine(staticFilesPath, "event-images");
+if (!Directory.Exists(imagesPath))
+    Directory.CreateDirectory(imagesPath);
+
+// SERVA statiska filer
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(staticFilesPath),
+    RequestPath = ""
+});
+
+app.UseCors("AllowFrontend");
 
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
@@ -54,12 +64,9 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
         context.Response.Redirect("/swagger");
         return Task.CompletedTask;
     });
-
-
 }
 
 app.UseHttpsRedirection();
-app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
 
